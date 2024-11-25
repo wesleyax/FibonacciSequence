9c85874a78dc44e9ee86dc01fc603571f19c7627
Wesley Ax <contact@wesleyax.com>
2024-11-25 18:57:31 +1300

    Final project

--- FibonacciSequence/Program.cs
+++ FibonacciSequence/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] fibonccini = new int[14];
+            fibonccini[0] = 0;
+            fibonccini[1] = 1;
+            int aux = 0;
+
+            for (int i = 2; i < 14; i++) {
+                fibonccini[i] = fibonccini[i-2] + aux;
+                aux = fibonccini[i];
+                Console.WriteLine(fibonccini[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
